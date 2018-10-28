62a0f102fc0ff01fe08d6bd131134295b0360ead
Roberto Rega <rega.web@gmail.com>
2018-10-28 14:17:58 +0100

    Test call rest for categoria by id - Categoria

--- src/main/java/com/neliocalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/neliocalves/cursomc/resources/CategoriaResource.java
@@ -3,16 +3,26 @@ package com.neliocalves.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.neliocalves.cursomc.domain.Categoria;
+import com.neliocalves.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource 
 {
+	
+	/** IoC */
+	@Autowired
+	private CategoriaService service;
+	
+	
 	/**
 	 * Converte in automatico la lista in
 	 * formato json:
@@ -33,5 +43,12 @@ public class CategoriaResource
 		
 		
 		return lista;
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) 
+	{
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
---
+++ src/main/java/com/neliocalves/cursomc/services/CategoriaService.java
@@ -0,0 +1,25 @@
+package com.neliocalves.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.neliocalves.cursomc.domain.Categoria;
+import com.neliocalves.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService 
+{
+	
+	/** definiamo dipendenza injection o
+	 * 		inversion of control
+	 */
+	@Autowired
+	private CategoriaRepository repo;
+	
+	
+	public Categoria buscar(Integer id) {
+		Categoria obj = repo.findOne(id);
+		return obj;
+	}
+
+}
